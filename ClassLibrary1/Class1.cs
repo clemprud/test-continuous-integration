ad2f6797dceb5e874efabfa80aba1064f0dfff20
Clément PRUDHOMME <c.prudhomme@proxiad.com>
2020-04-30 15:52:34 +0200

    Ajout méthode GetInt

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -4,5 +4,15 @@ namespace ClassLibrary1
 {
     public class Class1
     {
+        /// <summary>
+        /// Retourne un nombre donné sous forme d'une chaine de caractère
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <returns></returns>
+        public static int GetInt(string nombre)
+        {
+            var res = int.Parse(nombre);
+            return res + 1;            
+        }
     }
 }
